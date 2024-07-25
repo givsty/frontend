30516adde3858d73579057d5c9f5afa8bbf6cf7d
Igor <golovkovvigg@gmail.com>
2024-07-25 19:53:15 +0300

    ....

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
 import React from 'react'
-import basket from './images/header/basket.png'
-import favorites from './images/header/favorites.png'
-import brand from './images/header/brand.png'
+
 const App: React.FC = () => {
   return (
     <div className="wrapper">
       <header>
         <div className="header__left">
           <div className="header__left__logo">
-            <img width={40} height={40} src={brand} alt="" />
+            <img width={40} height={40} src="/images/header/brand.png" alt="" />
           </div>
           <div className="header__left__text">
             <h2>REACT SNEAKERS</h2>
@@ -17,15 +15,18 @@ const App: React.FC = () => {
         </div>
         <div className="header__right">
           <ul>
-            <li><img width={18} height={18}  src={basket} alt="" /> <span>1205 руб.</span> </li>
-            <li><img width={18} height={18}  src={favorites} alt="" /> <span>Закладки</span> </li>
-            <li><img width={18} height={18}  src="" alt="" /> <span>Профиль</span> </li>
+            <li><img width={18} height={18}  src="/images/header/basket.png" alt="" /> <span>1205 руб.</span> </li>
+            <li><img width={18} height={18}  src="/images/header/favorites.png" alt="" /> <span>Закладки</span> </li>
+            <li><img width={18} height={18}  src="/images/header/brand.png" alt="" /> <span>Профиль</span> </li>
           </ul>
         </div>
       </header>
       <div className="line"></div>
       <div className="wrapper__content">
-        <img src="" alt="" />
+        <div className="slider">
+          <img src="" alt="" />
+        </div>
+        <h1>Все кроссовки</h1>
       </div>
     </div>
   )
