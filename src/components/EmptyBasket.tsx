0fef0956cf6cb558c37754809d09b4bd26378d12
Igor <golovkovviggreserv@gmail.com>
2024-08-16 19:52:22 +0300

    fix empty basket styles

--- src/components/EmptyBasket.tsx
+++ src/components/EmptyBasket.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+
 interface EmptyBaskettProps {
   toggleBasket: () => void;
 }
+
 const EmptyBasket: React.FC<EmptyBaskettProps> = ({ toggleBasket }) => {
   return (
     <div className="empty__basket">
       <div className="empty__basket__wrapper">
+        <img src="/images/header/emptyBasket/Group 117.svg" width={120} height={120} alt="" />
         <h3>Корзина пустая</h3>
         <p>Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ</p>
         <button onClick={toggleBasket}>Вернуться назад</button>
@@ -15,4 +18,3 @@ const EmptyBasket: React.FC<EmptyBaskettProps> = ({ toggleBasket }) => {
 };
 
 export default EmptyBasket;
-
--- src/pages/Basket.tsx
+++ src/pages/Basket.tsx
@@ -14,6 +14,7 @@ const Basket: React.FC<BasketProps> = ({ toggleBasket }) => {
   const basketItems = useSelector(
     (state: { cart: { itemsInCart: ISneaker[] } }) => state.cart.itemsInCart
   );
+  
   useEffect(() => {
     console.log('useEffect сработал');
     if (isMounted.current) {
@@ -25,11 +26,12 @@ const Basket: React.FC<BasketProps> = ({ toggleBasket }) => {
       console.log('Первый рендер');
     }
   }, [basketItems]);
+
   return (
     <div className="overlay">
       <div className="drawer">
         <div className="drawer__close__btn" onClick={toggleBasket}>
-          <span>Назад</span>
+          <button>Назад</button>
         </div>
         <h3>Корзина</h3>
         {basketItems.length !== 0 ? basketItems.map((element, index) => {
